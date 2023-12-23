881b8ece3bca1681ee25937deaf5e84e5bb83a81
parallels <parallels@kali-gnu-linux-2023.2>
2023-12-22 18:31:33 -0800

    Fixed random

--- game_evaluation.sh
+++ game_evaluation.sh
@@ -7,7 +7,7 @@ echo "choose a number from 1 to 5"
 read num
 
 #generate a random number from 1 to 5
-r=$(( $RANDOM % 6  ))
+r=$(( (RANDOM % 5) + 1 ))
 
 #compare the input value with the random value
 if [ "$num" -eq "$r" ];then
